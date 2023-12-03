401723f9271dada6458cc180e3560fc9626caf95
Oscar Utbult <oscar.utbult@gmail.com>
2023-12-03 01:14:22 +0100

    day01 Terraform cleanup and improvement

--- day01/part2/terraform/main.tf
+++ day01/part2/terraform/main.tf
@@ -3,32 +3,26 @@ variable "filename" {
 }
 
 locals {
-  toDigit = tomap({
-    one       = "1"
-    two       = "2"
-    three     = "3"
-    four      = "4"
-    five      = "5"
-    six       = "6"
-    seven     = "7"
-    eight     = "8"
-    nine      = "9"
-    oneight   = "18",
-    twone     = "21",
-    threeight = "38",
-    fiveight  = "58",
-    eightwo   = "82",
-    eighthree = "83",
-    nineight  = "98"
+  wordToNumber = tomap({
+    zero  = "0"
+    one   = "1"
+    two   = "2"
+    three = "3"
+    four  = "4"
+    five  = "5"
+    six   = "6"
+    seven = "7"
+    eight = "8"
+    nine  = "9"
   })
+  wordsRegex = "one|two|three|four|five|six|seven|eight|nine"
 
   lines = split("\n", file("${path.module}/${var.filename}"))
 
-  # Due to limited regex capabilities in Terraform we can't use positive lookahead. Otherwise the following would have
-  # provided a better solution than the current hack: (?=(\d|one|two|three|four|five|six|seven|eight|nine))
-  digitsAndWords    = [for line in local.lines : regexall("\\d|oneight|twone|threeight|fiveight|eightwo|eighthree|nineight|one|two|three|four|five|six|seven|eight|nine", line)]
-  digits            = [for line in local.digitsAndWords : flatten([for item in line : try(split("", lookup(local.toDigit, item, item)), lookup(local.toDigit, item, item))])]
-  calibrationValues = [for line in local.digits : try(tonumber(format("%s%s", element(line, 0), element(line, length(line) - 1))), 0)]
+  # Due to the limited regex in Terraform we're forced to use this hack instead overlapping matches.
+  digitsAndWords    = [for line in local.lines : [regexall(format("\\d|%s", local.wordsRegex), line)[0], strrev(regexall(format("\\d|%s", strrev(local.wordsRegex)), strrev(line))[0])]]
+  digits            = [for line in local.digitsAndWords : [lookup(local.wordToNumber, line[0], line[0]), lookup(local.wordToNumber, line[1], line[1])]]
+  calibrationValues = [for line in local.digits : tonumber(format("%s%s", element(line, 0), element(line, length(line) - 1)))]
   result            = sum(local.calibrationValues)
 }
 
